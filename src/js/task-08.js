f6f3f958697aeb0dea92d10bc5849190e1c6e54e
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2023-02-22 21:22:49 +0100

    зробив 8 завдання

---
+++ src/js/task-08.js
@@ -0,0 +1,28 @@
+const form = document.querySelector(".login-form");
+
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log(`email: ${email.value}, password: ${password.value}`);
+  event.currentTarget.reset();
+    // const formData = new FormData(event.currentTarget) 
+    
+    // formData.forEach((value, name) => {
+    //     if (name === "" || value === "") {
+    //         return alert("Please fill in all the fields!");
+    //     }
+    //     console.log( name, value);
+       
+    // })
+    
+}
